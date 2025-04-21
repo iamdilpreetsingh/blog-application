6e34dadd18b6dc7451d710aca6184f7bd762ce17
Dilpreet Singh <dilpreetsingh3682@gmail.com>
2025-04-22 01:34:11 +0530

    chore: eslint config

---
+++ blog_api/eslint.config.js
@@ -0,0 +1,17 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "script",
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+];
